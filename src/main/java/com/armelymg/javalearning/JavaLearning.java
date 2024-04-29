a008a41a4333d375368a04647533055f94cf7cad
B. Armel YAMEOGO <baowendsommearmel@gmail.com>
2024-04-29 12:56:58 +0000

    Ajout d'une fonction de calcul de la superficie d'un rectangle

--- src/main/java/com/armelymg/javalearning/JavaLearning.java
+++ src/main/java/com/armelymg/javalearning/JavaLearning.java
@@ -13,4 +13,14 @@ public class JavaLearning {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    /*
+    * Méthode pour calculer la superficie d'un rectangle.
+    * @param longueur Longueur du rectangle
+    * @param largeur Largeur du rectangle
+    * @return float La superficie du rectangle
+    */
+    public float calculerSuperficieRRectangle( final float longueur, final float largeur){
+        return longueur*largeur;
+    }
 }
